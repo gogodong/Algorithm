bacbc8ecfdbe1002b85bbbbeee5cb84112a40c22
GuoZhendong <3389089691@qq.com>
2023-11-19 16:29:38 +0800

    加了一小段注释

--- week2/week2hw3-main_element.py
+++ week2/week2hw3-main_element.py
@@ -1,20 +1,20 @@
 import numpy as np
 
-arr = np.array([1,2,3,45,61,1,5,1])
+arr = np.array([1, 2, 3, 45, 61, 1, 5, 1])
 
-stk = []
+stk = []  # python的栈可以完全使用数组来解决
 top = 0
 
 for i in arr:
     stk.append(i)
     top = top + 1
     if top > 1:
-        if stk[top-1] != stk[top-2]:
+        if stk[top - 1] != stk[top - 2]:
             stk.pop()
             stk.pop()
             top = top - 2
 
-if len(stk)>0:
+if len(stk) > 0:
     print(stk[0])
 else:
     print("No Main Element!")
